421bab0733905dfcb5982dd34bf70063ff918a89
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-02-14 11:31:19 +0900

    after modifying connection error

--- websocket_vue/websocket_nuxt/plugins/vue-native-websocket.js
+++ websocket_vue/websocket_nuxt/plugins/vue-native-websocket.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueNativeSock from 'vue-native-websocket'
 
-Vue.use(VueNativeSock, 'ws://localhost:8000', {
-  connectManually: true,
+Vue.use(VueNativeSock, 'ws://15.164.96.121:8000', { //서버의 IP주소와 port기입
+    connectManually: true,
 })
